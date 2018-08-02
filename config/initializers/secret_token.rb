70d154b382f26e5542323474359e50686d2c146f
ArockiaHelanFw <arockia.helanmary@freshworks.com>
2018-08-02 16:12:48 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlaBlaCar::Application.config.secret_token = '214f18e52baa03468bedb134669581ea0fe04a95e85858467f2dc3e64750e52b6f0fe1c45f58c784841250357d52b3aae72ec60e19d924c4de04bba783b1f196'
